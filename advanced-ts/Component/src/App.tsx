06e5f2398ca5a96cb68eb8c9503c57887acdbd1e
Tung Phan <thanhtungphanvu@gmail.com>
2024-04-27 18:22:29 +0300

    initial form submission

--- advanced-ts/Component/src/App.tsx
+++ advanced-ts/Component/src/App.tsx
@@ -1,12 +1,19 @@
 import Input from "./components/Input";
 import Button from "./components/Button";
 import Container from "./components/Container";
+import Form from "./components/Form";
 import { useRef } from "react";
 function App() {
-  const input = useRef<HTMLInputElement>(null);
+
   return (
     <main>
-      <Input label="Test" id="test" ref={input} />
+      <Form>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
