c3e80cbe5d52fa7c4ce07b8b855d2704745e12bc
Tung Phan <thanhtungphanvu@gmail.com>
2024-04-27 14:27:06 +0300

    ComponentPropsWithoutRef

--- advanced-ts/Component/src/App.tsx
+++ advanced-ts/Component/src/App.tsx
@@ -2,8 +2,8 @@ import Input from "./components/Input";
 function App() {
   return (
     <main>
-      <Input id="name" label="Your name" />
-      <Input id="age" label="Your age" />
+      <Input id="name" label="Your name" type="text"/>
+      <Input id="age" label="Your age"  type="number"/>
     </main>
   );
 }
--- advanced-ts/Component/src/components/Input.tsx
+++ advanced-ts/Component/src/components/Input.tsx
@@ -1,12 +1,14 @@
+import { ComponentPropsWithoutRef } from "react";
+
 type InputProps = {
   label: string;
   id: string;
-};
-export default function Input(props: InputProps) {
+} & ComponentPropsWithoutRef<"input">;
+export default function Input({ label, id, ...props }: InputProps) {
   return (
     <p>
-      <label htmlFor={props.id}>{props.label}</label>
-      <input />
+      <label htmlFor={id}>{label}</label>
+      <input id={id} {...props} />
     </p>
   );
 }
