7bf839bf6295fbf6981ca202210518a25e290fcf
Tung Phan <thanhtungphanvu@gmail.com>
2024-04-29 09:56:05 +0300

    advanced typesafe

--- advanced-ts/Component/src/components/Form.tsx
+++ advanced-ts/Component/src/components/Form.tsx
@@ -1,17 +1,19 @@
-import { ComponentPropsWithoutRef, FormEvent } from "react";
+import { ComponentPropsWithoutRef, FormEvent, useRef } from "react";
 
 type FormProps = ComponentPropsWithoutRef<"form"> & {
   onSave: (value: unknown) => void;
 };
 export default function Form({ onSave, children, ...otherProps }: FormProps) {
+  const form = useRef<HTMLFormElement>(null);
   function HandleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const data = Object.fromEntries(formData);
     onSave(data);
+    form.current!.reset();
   }
   return (
-    <form onSubmit={HandleSubmit} {...otherProps}>
+    <form onSubmit={HandleSubmit} {...otherProps} ref={form}>
       {children}
     </form>
   );
