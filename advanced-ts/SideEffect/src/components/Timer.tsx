f35046758a3122af9a398aa77a5e04725f13eabd
Tung Phan <thanhtungphanvu@gmail.com>
2024-04-30 11:23:18 +0300

    update

--- advanced-ts/SideEffect/src/components/Timer.tsx
+++ advanced-ts/SideEffect/src/components/Timer.tsx
@@ -1,20 +1,32 @@
 import Container from "./UI/Container.tsx";
-import { type Timer as TimerProps } from "../store/timers-context.tsx";
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from "../store/timers-context.tsx";
 import { useState, useEffect, useRef } from "react";
 
 export default function Timer({ name, duration }: TimerProps) {
   const interval = useRef<number | null>(null);
   const [remainingState, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
   if (remainingState <= 0 && interval.current) {
     clearInterval(interval.current);
   }
   useEffect(() => {
-    const timer = setInterval(function () {
-      setRemainingTime((prevTime) => prevTime - 50);
-    }, 50);
-    interval.current = timer;
+    let timer: number;
+    
+    if (isRunning) {
+      timer = setInterval(function () {
+        setRemainingTime((prevTime) => prevTime - 50);
+      }, 50);
+      interval.current = timer;
+
+    } else if (interval.current) {
+      clearInterval(interval.current)
+    }
+
     return () => clearInterval(timer);
-  }, []);
+  }, [isRunning]);
   const formattedRemainingTime = (remainingState / 1000).toFixed(2);
   return (
     <Container as="article">
